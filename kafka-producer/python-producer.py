486f2c2791e1c8079578dbfa2a0373299fe160f5
62727 <62727@etu.he2b.be>
2026-03-03 09:03:42 +0100

    feat: - chore: remove build artifacts such as .class, target files, config files - python-producer.py: fix CSV column name mismatches - fix timestamp parser format in AISDataDeserializationSchema - small updates in TrajectoryWindowFunction & Main to simplify

--- kafka-producer/python-producer.py
+++ kafka-producer/python-producer.py
@@ -8,12 +8,19 @@ kafka_nodes = "kafka:29092"
 myTopic = "aisdata"
 
 def gen_data():
-  df = pd.read_csv('multi_ship_trajectory.csv')
+  df = pd.read_csv('ais_instants.csv')
 
   prod = KafkaProducer(bootstrap_servers=kafka_nodes, value_serializer=lambda x:dumps(x).encode('utf-8'))
 
   for index, row in df.iterrows():
-    my_data = {'t': row['t'], 'mmsi': row['mmsi'], 'lon': row['lon'], 'lat': row['lat'], 'speed': row['speed'], 'course': row['course']}
+    my_data = {
+    't': row['t'],
+    'mmsi': row['mmsi'],
+    'lon': row['longitude'],   # était row['lon']
+    'lat': row['latitude'],    # était row['lat']
+    'speed': row['sog'],       # était row['speed']
+    'course': 0.0              # n'existe pas dans le CSV, valeur par défaut
+}
     print(my_data)
     prod.send(topic=myTopic, value=my_data)
 
